366fd812fe21902621830222c656743617dffc3a
VanoKi <kirsvano@gmail.com>
2025-03-29 14:17:54 +0300

    fix: items in map

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -18,9 +18,9 @@ export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
 
       <ul>
         {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
-          <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>
-            <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>
-            {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}
+          <li key={user.id} id={`hw01-user-${user.id}`}>
+            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+            {user.address.street}, {user.address.city}
           </li>
         ))}
       </ul>
