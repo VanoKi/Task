c7e518e9e0837bf08cd40e7ac181431d38976e2d
VanoKi <kirsvano@gmail.com>
2025-04-01 17:34:08 +0300

    fix: other

--- src/script2.js
+++ src/script2.js
@@ -1,3 +1,3 @@
 const calcValues = (a, b) => [a + b, a- b, a/b, a*b]
-const [sum, , , mult] = calcValues(5, 6)
-console.log(sum, mult)
+const [sum, sub, ...other] = calcValues(5, 6)
+console.log(sum, sub, other)
