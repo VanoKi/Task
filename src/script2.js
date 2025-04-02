39e6d425b67d3b60c5b8239bb68a1cd4490a3321
VanoKi <kirsvano@gmail.com>
2025-04-02 19:00:42 +0300

    feat: shift

--- src/script2.js
+++ src/script2.js
@@ -1,10 +1,22 @@
-const person = {
-    name: 'Kirill',
-    age: 24,
-    adress: {
-        country: 'Poland',
-        city: 'Warsaw',
-    },
+const array = [1, 2, 3, 4, 5]
+
+function slam(arr, ...rest) {
+    console.log(rest)
 }
-const { name, age, car = 'person doesn`t have the car' } = person // Эквивалентно const name = person.name; const age = person.age;
-console.log(name, age, car)
+
+function _pop(arr) {
+    const last = arr[arr.length -1]
+    arr.length = arr.length - 1
+    return [last, arr]
+}
+
+function _shift(arr) {
+    const firstEl = arr[0]
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = arr[i + 1]
+    }
+    arr.length = arr.length - 1
+    return [firstEl, arr]
+}
+
+console.log(_shift(array))
