72f51a8eaf504c11e4dab92689f038e9d213a3c3
VanoKi <kirsvano@gmail.com>
2025-04-16 20:41:42 +0300

    feat: createTask

--- src/script4.js
+++ src/script4.js
@@ -20,18 +20,18 @@ const todolists = [
         ]
     },
 ]
-const tasks = [
-    {id: 11, tidolistId: 1, title: "HTML", isDone: true},
-    {id: 12, tidolistId: 1, title: "CSS", isDone: true},
-    {id: 13, tidolistId: 1, title: "JS/TS", isDone: false},
-    {id: 21, tidolistId: 2, title: "Beer", isDone: true},
-    {id: 22, tidolistId: 2, title: "Meat", isDone: true},
-    {id: 23, tidolistId: 2, title: "Bread", isDone: false}
-]
+// const tasks = [
+//     {id: 11, tidolistId: 1, title: "HTML", isDone: true},
+//     {id: 12, tidolistId: 1, title: "CSS", isDone: true},
+//     {id: 13, tidolistId: 1, title: "JS/TS", isDone: false},
+//     {id: 21, tidolistId: 2, title: "Beer", isDone: true},
+//     {id: 22, tidolistId: 2, title: "Meat", isDone: true},
+//     {id: 23, tidolistId: 2, title: "Bread", isDone: false}
+// ]
 const todoListId_1 = crypto.randomUUID()
 const todoListId_2 = crypto.randomUUID()
 
-const tasks2 = {
+const tasks = {
     [todoListId_1]: [{id: 11, title: "HTML", isDone: true},
         {id: 12, title: "CSS", isDone: true},
         {id: 13, title: "JS/TS", isDone: false},],
@@ -48,7 +48,19 @@ function getPropName(userName) {
 function createTodoList(title) {
     const newTodoListID = crypto.randomUUID()
     const newTodoList = {id: newTodoListID, title, filter}
+    const todolists = [...tasks2, newTodoList]
+    const tasks = {...tasks, [newTodoListID] : []}
 }
 
+function changeTaskStatus(todoListId, taskId, newStatus) {
+    // const tasks = tasks2[todoListId]
+    // const newTasks = tasks.map(t => t.id === taskId ? {...t, isDone: newStatus} : t)
+    // tasks = {...tasks, [todoListId] : newTasks}
+    return {...tasks, [todoListId] : tasks[todoListId].map(t => t.id === taskId ? {...t, isDone: newStatus} : t)}
+}
 
+function createTask(title, todoList) {
+    const newTask = {id: crypto.randomUUID(), title, isDone: false}
+    tasks = {...tasks, [todoList] : [...tasks[todoListId], newTask]}
+}
 
