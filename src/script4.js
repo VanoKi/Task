2a1085834c0c2f6cfbe14e88d24f904bd67cf5fa
VanoKi <kirsvano@gmail.com>
2025-04-16 19:44:08 +0300

    feat: counting

--- src/script4.js
+++ src/script4.js
@@ -28,13 +28,17 @@ const tasks = [
     {id: 22, tidolistId: 2, title: "Meat", isDone: true},
     {id: 23, tidolistId: 2, title: "Bread", isDone: false}
 ]
+const todoListId_1 = crypto.randomUUID()
+const todoListId_2 = crypto.randomUUID()
 
 const tasks2 = {
-    '1': [{id: 11, title: "HTML", isDone: true},
+    [todoListId_1]: [{id: 11, title: "HTML", isDone: true},
         {id: 12, title: "CSS", isDone: true},
         {id: 13, title: "JS/TS", isDone: false},],
-    '2': [{id: 21, title: "Beer", isDone: true},
+    [todoListId_2]: [{id: 21, title: "Beer", isDone: true},
         {id: 22, title: "Meat", isDone: true},
         {id: 23, title: "Bread", isDone: false}]
 }
 
+console.log(tasks2[todoListId_1])
+console.log(tasks2[todoListId_2])
