4754d453e221fabc0aa1a78fc657c9641811f500
VanoKi <kirsvano@gmail.com>
2025-04-16 19:15:03 +0300

    feat: create script4.js

---
+++ src/script4.js
@@ -0,0 +1,22 @@
+const todolists = [
+    {
+        id: 1,
+        title: "What to learn",
+        filter: "all",
+        tasks: [
+            { id: 11, title: "HTML", isDone: true },
+            { id: 12, title: "CSS", isDone: true },
+            { id: 13, title: "JS/TS", isDone: false }
+        ]
+    },
+    {
+        id: 2,
+        title: "What to buy",
+        filter: "all",
+        tasks: [
+            { id: 21, title: "Beer", isDone: true },
+            { id: 22, title: "Meat", isDone: true },
+            { id: 23, title: "Bread", isDone: false }
+        ]
+    },
+]
