48d783001115df0522ec85702489f025ebb9929a
VanoKi <kirsvano@gmail.com>
2025-04-16 19:26:12 +0300

    feat: move to object

--- src/script4.js
+++ src/script4.js
@@ -4,9 +4,9 @@ const todolists = [
         title: "What to learn",
         filter: "all",
         tasks: [
-            { id: 11, title: "HTML", isDone: true },
-            { id: 12, title: "CSS", isDone: true },
-            { id: 13, title: "JS/TS", isDone: false }
+            {id: 11, title: "HTML", isDone: true},
+            {id: 12, title: "CSS", isDone: true},
+            {id: 13, title: "JS/TS", isDone: false}
         ]
     },
     {
@@ -14,18 +14,27 @@ const todolists = [
         title: "What to buy",
         filter: "all",
         tasks: [
-            { id: 21, title: "Beer", isDone: true },
-            { id: 22, title: "Meat", isDone: true },
-            { id: 23, title: "Bread", isDone: false }
+            {id: 21, title: "Beer", isDone: true},
+            {id: 22, title: "Meat", isDone: true},
+            {id: 23, title: "Bread", isDone: false}
         ]
     },
 ]
 const tasks = [
-    { id: 11, tidolistId: 1, title: "HTML", isDone: true },
-    { id: 12, tidolistId: 1, title: "CSS", isDone: true },
-    { id: 13, tidolistId: 1, title: "JS/TS", isDone: false },
-    { id: 21, tidolistId: 2, title: "Beer", isDone: true },
-    { id: 22, tidolistId: 2, title: "Meat", isDone: true },
-    { id: 23, tidolistId: 2, title: "Bread", isDone: false }
+    {id: 11, tidolistId: 1, title: "HTML", isDone: true},
+    {id: 12, tidolistId: 1, title: "CSS", isDone: true},
+    {id: 13, tidolistId: 1, title: "JS/TS", isDone: false},
+    {id: 21, tidolistId: 2, title: "Beer", isDone: true},
+    {id: 22, tidolistId: 2, title: "Meat", isDone: true},
+    {id: 23, tidolistId: 2, title: "Bread", isDone: false}
 ]
+
+const tasks2 = {
+    '1': [{id: 11, tidolistId: 1, title: "HTML", isDone: true},
+        {id: 12, tidolistId: 1, title: "CSS", isDone: true},
+        {id: 13, tidolistId: 1, title: "JS/TS", isDone: false},],
+    '2': [{id: 21, tidolistId: 2, title: "Beer", isDone: true},
+        {id: 22, tidolistId: 2, title: "Meat", isDone: true},
+        {id: 23, tidolistId: 2, title: "Bread", isDone: false}]
+}
 
