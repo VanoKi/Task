c5b850645e580ba10c95b05454252078c78a0f1e
VanoKi <kirsvano@gmail.com>
2025-04-16 19:24:19 +0300

    feat: add todoListId

--- src/script4.js
+++ src/script4.js
@@ -20,3 +20,12 @@ const todolists = [
         ]
     },
 ]
+const tasks = [
+    { id: 11, tidolistId: 1, title: "HTML", isDone: true },
+    { id: 12, tidolistId: 1, title: "CSS", isDone: true },
+    { id: 13, tidolistId: 1, title: "JS/TS", isDone: false },
+    { id: 21, tidolistId: 2, title: "Beer", isDone: true },
+    { id: 22, tidolistId: 2, title: "Meat", isDone: true },
+    { id: 23, tidolistId: 2, title: "Bread", isDone: false }
+]
+
