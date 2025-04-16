b51fce938fa61235b79bac062e8f91d508c507f1
VanoKi <kirsvano@gmail.com>
2025-04-16 19:37:09 +0300

    fix: todoListId

--- src/script4.js
+++ src/script4.js
@@ -30,11 +30,11 @@ const tasks = [
 ]
 
 const tasks2 = {
-    '1': [{id: 11, tidolistId: 1, title: "HTML", isDone: true},
-        {id: 12, tidolistId: 1, title: "CSS", isDone: true},
-        {id: 13, tidolistId: 1, title: "JS/TS", isDone: false},],
-    '2': [{id: 21, tidolistId: 2, title: "Beer", isDone: true},
-        {id: 22, tidolistId: 2, title: "Meat", isDone: true},
-        {id: 23, tidolistId: 2, title: "Bread", isDone: false}]
+    '1': [{id: 11, title: "HTML", isDone: true},
+        {id: 12, title: "CSS", isDone: true},
+        {id: 13, title: "JS/TS", isDone: false},],
+    '2': [{id: 21, title: "Beer", isDone: true},
+        {id: 22, title: "Meat", isDone: true},
+        {id: 23, title: "Bread", isDone: false}]
 }
 
