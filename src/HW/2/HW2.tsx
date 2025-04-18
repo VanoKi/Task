bbf63cd9d2e29f44409d683b9f9ec25faabfe758
VanoKi <kirsvano@gmail.com>
2025-04-18 13:09:53 +0300

    fix: filterUsers HW2.tsx

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -47,7 +47,7 @@ export const HW2 = () => {
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
 
   const filterUsers = () => {
-    const filteredUsers = myFriends.filter(item => item.address.city === 'Los Angeles');
+    const filteredUsers = users.myFriends.filter(item => item.address.city === 'Los Angeles');
     setCurrentUsers({ myFriends: filteredUsers });
   };
 
